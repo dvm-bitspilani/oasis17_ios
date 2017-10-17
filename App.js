3bd0956d548ba3b466240cf6f2c7e58dff658a9f
Chinmay Pandhare <pandharechinmay882@gmail.com>
2017-10-17 10:19:27 +0530

    Added Navigator

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+class Home extends React.Component {
+  static navigationOptions = {
+    title: "Oasis 2017"
+  }
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text>Hello World</Text>
+        <Image source={require('./logo.png')} />
+        <Text style={styles.smallText}>Ab ki Baar iOS par bhi</Text>
+        <Button style={styles.smallText} onPress={()=>navigate('Demo')} title="Click Here" /> 
       </View>
     );
   }
 }
+
+class DemoScreen extends React.Component {
+  static navigationOptions = {
+    title: "Demo Screen"
+  }
+  render() {
+    return(
+      <Text>This is a Demo Screen.</Text>
+    );
+  }
+}
+
+const MainApp = StackNavigator({
+  Home: { screen: Home },
+  Demo: { screen: DemoScreen }
+}); 
+
+export default class App extends React.Component {
+  render() {
+    return <MainApp />;
+  }
+ }
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +46,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  },  
+  bigText: {
+    fontSize: 35  
+  },  
+  smallText: {
+    fontSize: 15  
+  }
 });
